4ccd32eb84afb58d4903e58bc8840ab4040e09ee
pugaivan <unsaightly@gmail.com>
2022-05-04 17:02:52 +0300

    add create route

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors")
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json())
+app.use(cors())
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+app.post('/create', (req, res) => {
+  console.log(req.body)
+  res.sendStatus(200)
+})
